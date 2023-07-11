4d9840def21b18ba990f4beed6c5f6c63a65910e
netos23 <nikmoroz2002@outlook.com>
2023-07-12 01:21:16 +0300

    init commit

---
+++ android/app/src/main/kotlin/ru/fbtw/rick_and_morty/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.fbtw.rick_and_morty
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/rick_and_morty_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RickAndMortyApp extends StatelessWidget {
+  const RickAndMortyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
