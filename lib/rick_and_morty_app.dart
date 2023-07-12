19f1db725c4a102eda4e9dd74bb024e44e9a6595
Morgoth228 <Dogman113636@gmail.com>
2023-07-12 19:25:33 +0300

    Сделал json обработчки однако он выдает ошибку

--- lib/rick_and_morty_app.dart
+++ lib/rick_and_morty_app.dart
@@ -1,24 +1,28 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty/model/future_updater.dart';
+import 'package:rick_and_morty/model/location/location.dart';
+import 'package:rick_and_morty/service/service.dart';
 
 class RickAndMortyApp extends StatelessWidget {
   const RickAndMortyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
+    final dio = Dio()..options.baseUrl = 'https://rickandmortyapi.com/api';
+    final client = RestClient(dio);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Location'),
       ),
       body:
       FutureUpdater(
-        future: Future(() async {
-          await Future.delayed(Duration(seconds: 3));
-          return 'Complite';
-        }),
+        future: client.getLocation(),
         builder: (context, data) {
-          data ??= '-1';
-          return Text(data);
+          data??=[];
+          return ListView.builder(itemCount: data.length, itemBuilder: (context, index){
+            return Text(data![index].toString());
+          });
         },
         loadingBuilder: (context) {
           return Center(
@@ -26,6 +30,7 @@ class RickAndMortyApp extends StatelessWidget {
           );
         },
         errorBuilder: (context, error, data){
+          print(error.toString());
           return Text(error.toString());
         },
       ),
---
+++ lib/service/service.g.dart
@@ -0,0 +1,108 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'service.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+
+class _RestClient implements RestClient {
+  _RestClient(
+    this._dio, {
+    this.baseUrl,
+  }) {
+    baseUrl ??= 'https://rickandmortyapi.com/api';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<List<Location>> getLocation() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result =
+        await _dio.fetch<List<dynamic>>(_setStreamType<List<Location>>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/location',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    var value = _result.data!
+        .map((dynamic i) => Location.fromJson(i as Map<String, dynamic>))
+        .toList();
+    return value;
+  }
+
+  @override
+  Future<Location> getSingleLocation(int id) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result =
+        await _dio.fetch<Map<String, dynamic>>(_setStreamType<Location>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/location/${id}',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = Location.fromJson(_result.data!);
+    return value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}
