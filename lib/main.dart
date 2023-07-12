a6751c32d560db4f1a5d7aaf360792ea028b0648
Morgoth228 <Dogman113636@gmail.com>
2023-07-12 17:58:01 +0300

    Сделена FutureUpdater А так же freezed

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:rick_and_morty/rick_and_morty_app.dart';
 
 void main() {
-  runApp(const RickAndMortyApp());
+  runApp(
+    MaterialApp(home: const RickAndMortyApp()),
+  );
 }
--- lib/rick_and_morty_app.dart
+++ lib/rick_and_morty_app.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty/model/future_updater.dart';
 
 class RickAndMortyApp extends StatelessWidget {
   const RickAndMortyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Location'),
+      ),
+      body:
+      FutureUpdater(
+        future: Future(() async {
+          await Future.delayed(Duration(seconds: 3));
+          return 'Complite';
+        }),
+        builder: (context, data) {
+          data ??= '-1';
+          return Text(data);
+        },
+        loadingBuilder: (context) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+        errorBuilder: (context, error, data){
+          return Text(error.toString());
+        },
+      ),
+    );
   }
 }
