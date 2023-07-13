a49cd231404478f3943b6768b8f9f2bc8b26374f
DaniilShayd <danyh852@gmail.com>
2023-07-13 19:17:32 +0300

    I have finalized my version of the application. In the future, the dio http client will be integrated.

--- lib/FUTURE_WIDGET/future_widget.dart
+++ lib/FUTURE_WIDGET/future_widget.dart
@@ -1,12 +1,8 @@
-/* Я дурик и не понял, что должен наш FUTURE виджет по условию делать,
-*  Возможно, мне нужно было сделать свою реализацию класса
-*  AsyncSnapshot, но опять же, непонятно как, ведь можно просто через зажатый ctrl
-*  Посмотреть его внутренности, ЕЩЁ РАЗ СОРРИ, БЬЮ ЧЕЛОМ, сделал простенький виджет,
-*  который принимает данные, а до их принятия крутит значок загрузки. Ы
+/* Еее,
 * */
-
+/*
+import 'dart:async';
 import 'package:flutter/material.dart';
-
 class FutureWidget<T> extends StatelessWidget {
   final Future<T> futureData;
 
@@ -30,3 +26,87 @@ class FutureWidget<T> extends StatelessWidget {
     );
   }
 }
+
+
+
+
+typedef LoadingBuilder = Widget Function(BuildContext);
+typedef DataBuilder<T> = Widget Function(BuildContext, T?);
+typedef ErrorBuilder<T> = Widget Function(BuildContext, dynamic, T?);
+
+class StreamUpdater<T> extends StatefulWidget {
+  const StreamUpdater({
+    super.key,
+    required this.stream,
+    required this.builder,
+    this.loadingBuilder,
+    this.errorBuilder,
+  });
+
+  final LoadingBuilder? loadingBuilder;
+  final DataBuilder<T> builder;
+  final ErrorBuilder<T>? errorBuilder;
+  final Stream<T> stream;
+
+  @override
+  State<StreamUpdater<T>> createState() => _StreamUpdaterState<T>();
+}
+
+class _StreamUpdaterState<T> extends State<StreamUpdater<T>> {
+  T? _data;
+  dynamic _error;
+  StreamSubscription? _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _subscription = widget.stream.listen(
+      _handleEvent,
+      onError: _handleError,
+    );
+  }
+
+  _handleError(error) {
+    setState(() {
+      _error = error;
+    });
+  }
+
+  void _handleEvent(event) {
+    setState(() {
+      _data = event;
+    });
+  }
+
+  @override
+  void didUpdateWidget(covariant StreamUpdater<T> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _subscription?.cancel();
+    _subscription = widget.stream.listen(
+      _handleEvent,
+      onError: _handleError,
+    );
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final errorBuilder = widget.errorBuilder;
+    if (_error != null && errorBuilder != null) {
+      return errorBuilder(context, _error, _data);
+    }
+
+    final loadingBuilder = widget.loadingBuilder;
+    if (_data == null && loadingBuilder != null) {
+      return loadingBuilder(context);
+    }
+
+    return widget.builder(context, _data);
+  }
+}
+*/
